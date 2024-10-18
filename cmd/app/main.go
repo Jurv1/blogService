448633440dc597152bd26f56f2e0aa68492456e5
jurv1 <bushmanhik931@mail.ru>
2024-10-18 17:07:18 +0300

    fast commit

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"context"
+	"fmt"
 	blogv1 "github.com/Jurv1/blogService/proto/gen/go/blog"
+	"github.com/Jurv1/blogService/proto/gen/go/user"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"log"
 	"net"
 )
 
@@ -11,22 +15,68 @@ type server struct {
 	blogv1.UnimplementedBlogServiceServer
 }
 
+type userServer struct {
+	user.UnimplementedUserServiceServer
+}
+
 func (s *server) CreateBlog(ctx context.Context, in *blogv1.CreateBlogRequest) (*blogv1.CreateBlogResponse, error) {
 	return nil, nil
 }
 
 func (s *server) GetBlog(ctx context.Context, in *blogv1.GetBlogRequest) (*blogv1.GetBlogResponse, error) {
+	client := getConn()
+
+	c := make(chan *user.MakeMutationResponse)
+	defer close(c)
+
+	go getResp(ctx, client, c)
+
+	//response, err := client.MutateUser(ctx, &user.MakeMutationRequest{
+	//	Message: "Mutate",
+	//})
+	//if err != nil {
+	//	return nil, err
+	//}
+	val, ok := <-c
+
+	if ok {
+		log.Println(fmt.Sprintf("AS %b", ok))
+	}
+
+	for {
+
+	}
+
+	log.Println(val)
+
+	//log.Println(fmt.Sprintf("sssss %s", response))
 
 	return &blogv1.GetBlogResponse{
-		Name: "new Blog",
+		Name: val.GetMessage(),
 	}, nil
 }
 
-func (s *server) GetBlogs(ctx context.Context, in *blogv1.GetBlogsRequest) (*blogv1.GetBlogsResponse, error) {
-	return nil, nil
+func getConn() user.UserServiceClient {
+	conn, err := grpc.NewClient("localhost:8080", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+	}
+
+	return user.NewUserServiceClient(conn)
 }
 
-func (s *server) MakeMutation(ctx context.Context, in *blogv1.MakeMutationRequest) (*blogv1.MakeMutationResponse, error) {
+func getResp(ctx context.Context, client user.UserServiceClient, c chan *user.MakeMutationResponse) *user.MakeMutationResponse {
+	response, err := client.MutateUser(ctx, &user.MakeMutationRequest{
+		Message: "Mutate",
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	c <- response
+
+	return response
+}
+
+func (s *server) GetBlogs(ctx context.Context, in *blogv1.GetBlogsRequest) (*blogv1.GetBlogsResponse, error) {
 	return nil, nil
 }
 
